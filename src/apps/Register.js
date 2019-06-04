126300fa92e546be21506bbf8e69df7653246dcb
Josue <josue.rivera@partnerhero.com>
2019-06-03 18:27:19 -0600

    Added Register Page

--- src/apps/Register.js
+++ src/apps/Register.js
@@ -1,29 +1,81 @@
 import React, {Component} from 'react';
-import charactersImage from '../characters.jpg';
 
 /* CSS Files */
 import '../css/Register.css';
 
 
 class RegisterApp extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            firstName: "",
+            lastName: "",
+            email: "",
+            password: ""
+        };
+
+        this.changeEvent = this.changeEvent.bind(this);
+        this.submitData = this.submitData.bind(this);
+    }
+
+    changeEvent(event) {
+        let name = event.target.name;
+
+        this.setState({
+            [name]: event.target.value
+        });
+    }
+
+    submitData(event) {
+        const url = "https://cors-anywhere.herokuapp.com/https://expense-challenge.herokuapp.com/register";
+        const postData = {
+            'email': this.state.email,
+            'password': this.state.password,
+            'first_name': this.state.firstName,
+            'last_name': this.state.lastName
+        }
+
+        fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((response) => {
+            return response.json()
+        }).then((jsonData) => {
+            if (jsonData["status"] === "success") {
+                // Redirect to login
+                window.location.replace("/login");
+            }
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+
     render() {
         return (
             <div className="register-app">
-                <div class="main-container">
+                <div className="main-container">
                     <div className="left-box">
                     </div>
                     <div className="right-box">
                         <h3 className="register-header">Create your account</h3>
-                        <label className="register-label">First Name</label>
-                        <input type="text" className="input register-input" placeholder="First Name"/>
-                        <label className="register-label">Last Name</label>
-                        <input type="text" class="input register-input" placeholder="Last Name"/>
-                        <label className="register-label">Email</label>
-                        <input type="email" className="input register-input" placeholder="Email"/>
-                        <label className="register-label">Password</label>
-                        <input type="password" className="input register-input" placeholder="Password" />
 
-                        <button id="register-submit-btn" className="button is-medium" type="submit">Create Account</button>
+                        <label htmlFor="firstName" className="register-label">First Name</label>
+                        <input value={this.firstName} onChange={this.changeEvent} name="firstName" type="text" className="input register-input" placeholder="First Name"/>
+
+                        <label htmlFor="lastName" className="register-label">Last Name</label>
+                        <input value={this.lastName} onChange={this.changeEvent} name="lastName" type="text" className="input register-input" placeholder="Last Name"/>
+
+                        <label htmlFor="email" className="register-label">Email</label>
+                        <input value={this.email} onChange={this.changeEvent} name="email" type="email" className="input register-input" placeholder="Email"/>
+
+                        <label htmlFor="password" className="register-label">Password</label>
+                        <input value={this.password} onChange={this.changeEvent} name="password" type="password" className="input register-input" placeholder="Password" />
+
+                        <button onClick={this.submitData} id="register-submit-btn" className="button is-medium" type="submit">Create Account</button>
                     </div>
                 </div>
             </div>
