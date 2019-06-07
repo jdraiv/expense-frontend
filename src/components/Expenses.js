88b7876aa9a0b2f48cf4041543586b43aab14c44
Josue <josue.rivera@partnerhero.com>
2019-06-06 20:39:04 -0600

    Added functionality to render expenses on the dashboard

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -6,8 +6,27 @@ import '../css/Expenses.css';
 import CreateExpenseMenu from './CreateExpenseMenu';
 
 
+class ExpenseTableItem extends Component {
+    // Props = color, category, total, payee, date
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        let color = {"backgroundColor": this.props.color};
+        return (
+            <tr>
+                <td className="table-category"><button className="table-category-btn" style={color}>{this.props.category}</button></td>
+                <td className="table-payee">{this.props.payee}</td>
+                <td className="table-total">${this.props.total}</td>
+                <td className="table-date">{this.props.date}</td>
+            </tr>
+        )
+    }
+}
+
+
 class Expenses extends Component {
-    // Props = authorizationKeys, updateTokensMethod
+    // Props = expenses, authorizationKeys, updateTokensMethod
     constructor(props) {
         super(props);
 
@@ -32,6 +51,9 @@ class Expenses extends Component {
     }
 
     render() {
+        let expenses = this.props.expenses.map((expense, key) => {
+            return <ExpenseTableItem color={expense['color']} category={expense['category']} payee={expense['payee']} total={expense['total']} date={expense['expenseDate']} />
+        });
         if (this.state.creatingExpense) {
             return (
                 <CreateExpenseMenu authorizationKeys={this.props.authorizationKeys} updateTokensMethod={this.props.updateTokensMethod} successFunction={this.expenseSuccessfullyCreated} closeMenuMethod={this.showOrHideMenu} />
@@ -39,23 +61,24 @@ class Expenses extends Component {
         }
         else {
             return (
-                <div className="expenses-container">
+                <div className="expenses-section">
                     <div className="budget-container">
-                        <h2 id="budget-value">Budget: {this.props.budget}</h2>
+                        <h2 id="budget-value">Budget: ${this.props.budget}</h2>
                     </div>
 
-                    <div className="expenses-container">
-                        <table>
-                            <tr>
+                    <div className="expenses-table-container">
+                        <table className="table expenses-table">
+                            <tr className="tbody">
                                 <th>Category</th>
                                 <th>Payee</th>
                                 <th>Total</th>
                                 <th>Date</th>
                             </tr>
+                            {expenses}
                         </table>
                     </div>
 
-                    <div className="button" onClick={this.showOrHideMenu}>Create Expense</div>
+                    <div id="create-expense-submit-btn" className="button" onClick={this.showOrHideMenu}>Create Expense</div>
                 </div>
             )
         }
